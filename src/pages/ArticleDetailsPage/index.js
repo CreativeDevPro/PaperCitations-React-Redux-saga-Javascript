658d51a467588a98e36cde44eaa286b056b28806
bestfriend815 <bestfriend20202020@outlook.com>
2020-12-16 08:29:21 +0300

    data format for graph

--- src/pages/ArticleDetailsPage/index.js
+++ src/pages/ArticleDetailsPage/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 import RelatedDoisList from './../../components/RelatedDoisList'
 import RelatedDoiDetail from './../../components/RelatedDoiDetail'
+import { connect } from 'react-redux';
 
+const ArticleDetailsPage = (props) => {
+  const { relatedDoiState, currentOriginalPaper } = props;
+  let links = []
+  let nodes = []
+  relatedDoiState.map(citation => {
+    links = [...links, {
+      "source": currentOriginalPaper.doi,
+      "target": citation.citing,
+      "value" : 1
+    }]
+    nodes = [...nodes, {
+      "id": citation.citing,
+      "year": citation.year,
+      "doi": citation.citing,
+      "title": citation.citing,
+      "authors": "",
+    }]
+  })
 
-const ArticleDetailsPage = () => (
-  <div>
-    <RelatedDoisList />
-    <RelatedDoiDetail />
-  </div>
-);
+  console.log(links);
+  console.log(nodes);
+
+  return (
+    <div>
+      <RelatedDoisList />
+      <RelatedDoiDetail />
+    </div>
+  )
+};
+
+const mapStateToProps=(state)=>{
+
+  return {
+    relatedDoiState: state.relatedDoiState,
+    currentOriginalPaper: state.currentOriginalPaper,
+  }
+}
+
+const mapStateToDispatch=(dispatch)=>{
+  return {
+    
+  }
+}
+
 
 ArticleDetailsPage.propTypes = {};
 
 ArticleDetailsPage.defaultProps = {};
 
-export default ArticleDetailsPage;
+export default connect(mapStateToProps, mapStateToDispatch)(ArticleDetailsPage);
