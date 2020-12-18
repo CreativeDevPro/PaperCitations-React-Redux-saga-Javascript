ded5c8a57eb44184d4be101b20296c4f6da1944d
bestfriend815 <bestfriend20202020@outlook.com>
2020-12-18 11:17:07 +0300

    fixed crash

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,14 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducer';
 import {createLogger} from 'redux-logger';
 
-const logger=createLogger();
+// const logger=createLogger();
 
 ////////////////////////
 export default function configureStore(initialState) {
 
     const sagaMiddleware = createSagaMiddleware();
     return {
-      ...createStore(rootReducer, initialState, applyMiddleware(logger,sagaMiddleware)),
+      ...createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware)),
        runSaga: sagaMiddleware.run
      }
   }
